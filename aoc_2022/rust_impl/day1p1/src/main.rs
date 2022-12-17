3b732f71777e90a27203654172b4d5eb1fe499fa
Balamurugan <emailstorbala@gmail.com>
2022-12-17 14:56:39 +0530

    Fixed line spacing, mixed chars, etc

--- aoc_2022/rust_impl/day1p1/src/main.rs
+++ aoc_2022/rust_impl/day1p1/src/main.rs
@@ -9,43 +9,42 @@ where P: AsRef<Path>, {
 }
 
 fn read_input_file(inp: String) -> Vec<i64> {
-	let mut res = Vec::new();
-	let mut tmp = 0;
+    let mut res = Vec::new();
+    let mut tmp = 0;
 
-	if let Ok(lines) = read_lines(inp) {
-		for line in lines {
-			if let Ok(ip) = line {
-				if ip.is_empty() {
-					res.push(tmp);
-					tmp = 0;
-				} else {
-					tmp += ip.parse::<i64>().unwrap();
-				}	
-			}
-		}
+    if let Ok(lines) = read_lines(inp) {
+        for line in lines {
+            if let Ok(ip) = line {
+                if ip.is_empty() {
+                    res.push(tmp);
+                    tmp = 0;
+                } else {
+                    tmp += ip.parse::<i64>().unwrap();
+                }
+            }
+        }
+        res.push(tmp);
+    }
 
-		res.push(tmp);
-	}
-
-	return res;
+    return res;
 }
 
 fn get_max_of(vec: Vec<i64>) -> i64 {
-	let mut max: i64 = 0; 
-	for num in vec {
-		if max >= num {
-			continue
-		} else {
-			max = num;
-		}
-	}
+    let mut max: i64 = 0;
+    for num in vec {
+        if max >= num {
+            continue
+        } else {
+            max = num;
+        }
+    }
 
-	return max;
+    return max;
 }
 
 fn main() {
     let inp = String::from("inp_file.txt");
     let ctx = read_input_file(inp);
-	let max = get_max_of(ctx);
-	println!("num is {}", max);
+    let max = get_max_of(ctx);
+    println!("num is {}", max);
 }
