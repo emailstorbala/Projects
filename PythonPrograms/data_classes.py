082456971e450e41807c163a2d27696b0dba2469
Balamurugan R <balamurugan.r@velocix.com>
2022-12-22 19:46:08 +0530

    Sample python data_classes explanation program added

---
+++ PythonPrograms/data_classes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Program to illustrate data classes
+"""
+import sys
+from dataclasses import dataclass
+
+@dataclass
+class Person:
+    """
+    Person class
+    """
+    name: str
+    age: int
+    address: str
+
+def main() -> None:
+    """
+    Main function
+    """
+    person = Person(name="Balamurugan", age=39, address="Tamilnadu")
+    print(person)
+
+if __name__ == '__main__':
+    sys.exit(main())
