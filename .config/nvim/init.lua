fd249abe0cee20cabc114f9d86e0d97a2416bce8
Balamurugan R <emailstorbala@gmail.com>
2023-01-07 20:17:13 +0530

    Fixed lint and mypy issues in migrate_p4_to_git.py. Removed unused nvim settings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -110,21 +110,14 @@ vim.opt.listchars = {
 -- Set highlight on search
 vim.o.hlsearch = true
 
--- Make line numbers default
-vim.wo.number = false
-
 -- Enable mouse mode
 vim.o.mouse = 'a'
-
--- Enable break indent
-vim.o.breakindent = true
-
--- Save undo history
-vim.o.undofile = true
 
 -- Case insensitive searching UNLESS /C or capital in search
 vim.o.ignorecase = true
 vim.o.smartcase = true
+vim.opt.cursorcolumn = true
+vim.opt.textwidth = 100
 
 -- Decrease update time
 vim.o.updatetime = 250
@@ -300,6 +293,7 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
 -- Custom user keymaps
 vim.keymap.set('n', '<leader>n', ":set number!<CR>")
 vim.keymap.set('n', '<leader>p', ":e#<CR>")
+vim.keymap.set('n', '<leader>l', ":set list!<CR>")
 
 -- LSP settings.
 --  This function gets run when an LSP connects to a particular buffer.
--- migrate_p4_to_git.py
+++ migrate_p4_to_git.py
@@ -9,14 +9,18 @@ instead of Shell.
 *** Program intended to be used as standalone. ***
 """
 
-import os
+from multiprocessing import Process
+from subprocess import Popen, PIPE, STDOUT
+from typing import List
 import argparse
 import logging
-from subprocess import Popen, PIPE, STDOUT
-from multiprocessing import Process
-from typing import List
+import os
+import sys
 
-class GLOBALS:
+class GLOBALS: # pylint:disable=too-few-public-methods
+    """
+    Global class
+    """
     GIT_P4_BIN = '/usr/local/bin/git-p4.py'
     LOGGER_FILE = 'p4_git_migrate.log'
 
@@ -39,72 +43,73 @@ def parse_arguments() -> argparse.Namespace:
     parser.add_argument('--doc_grp',
                         required=True,
                         help='Gitlab documentation group name')
-    args = parser.parse_args()
-    return args
+    return parser.parse_args()
 
 def validate_and_parse_file(inpfile:str) -> dict:
+    """ Method to validate and parse input file """
     p4_git_map:dict = {}
     inp_ctx:List[str] = []
 
-    with open(inpfile) as fp:
-        inp_ctx = [ line.strip() for line in fp.readlines() if not line.startswith('#') ]
+    with open(file=inpfile, encoding='utf-8') as inp_hdl:
+        inp_ctx = [ line.strip() for line in inp_hdl.readlines() if not line.startswith('#') ]
 
     for curr_line in inp_ctx:
-        p4_path, git_branch = curr_line.split(',')
+        _p4_path, _git_branch = curr_line.split(',')
 
-        if not p4_path.startswith("//"):
-            exception = "P4 path '{}' not starting with //".format(p4_path)
+        if not _p4_path.startswith("//"):
+            exception = f"P4 path '{_p4_path}' not starting with //"
             raise Exception(exception)
-        elif not p4_path.endswith("..."):
-            exception = "P4 path '{}' not ending with ...".format(p4_path)
+        if not _p4_path.endswith("..."):
+            exception = f"P4 path '{_p4_path}' not ending with ..."
             raise Exception(exception)
-        elif p4_path in p4_git_map.keys():
-            exception = "P4 path '{}' duplicates in the input file".format(p4_path)
+        if _p4_path in p4_git_map:
+            exception = f"P4 path '{_p4_path}' duplicates in the input file"
             raise Exception(exception)
 
-        p4_git_map[p4_path] = git_branch
+        p4_git_map[_p4_path] = _git_branch
 
     return p4_git_map
 
-def clone_from_p4(args, p4_path:str, git_branch:str):
+def clone_from_p4(_args:argparse.Namespace, _p4_path:str, _git_branch:str):
     """
     Based on the p4-git path mapping provided
     in the input file, this function will
-    git clone from the p4_path.
+    git clone from the _p4_path.
     """
-    repo_name:str = p4_path.split('/')[-2]
+    repo_name:str = _p4_path.split('/')[-2]
     wrk_dir:str = os.path.dirname(os.path.abspath(__file__))
-    clone_dir:str = "{}/{}_{}".format(wrk_dir, repo_name, git_branch)
+    clone_dir:str = f"{wrk_dir}/{repo_name}_{_git_branch}"
     cmd_execpath_map:dict = {}
-    
-    # Frame commands for migrating the p4_path to git
-    cmd = f"{GLOBALS.GIT_P4_BIN} clone --destination={clone_dir} {p4_path}@all"
+
+    # Frame commands for migrating the _p4_path to git
+    cmd = f"{GLOBALS.GIT_P4_BIN} clone --destination={clone_dir} {_p4_path}@all"
     cmd_execpath_map[1] = (cmd, wrk_dir)
 
-    if git_branch != "master":
-        cmd = f"git checkout -b {git_branch}"
+    if _git_branch != "master":
+        cmd = f"git checkout -b {_git_branch}"
         cmd_execpath_map[2] = (cmd, clone_dir)
-    
-    cmd = f"git remote add origin {args.gitlab_endpoint}:{args.root_group}/{args.doc_grp}/{repo_name}.git"
+
     cmd = (
             "git remote add origin "
-            f"{args.gitlab_endpoint}:{args.root_group}/${args.doc_grp}/{repo_name}.git"
-          )
+            f"{_args.gitlab_endpoint}:{_args.root_group}/${_args.doc_grp}/{repo_name}.git"
+            )
     cmd_execpath_map[3] = (cmd, clone_dir)
 
-    cmd = f"git push origin {git_branch}"
+    cmd = f"git push origin {_git_branch}"
     cmd_execpath_map[4] = (cmd, clone_dir)
 
     for (cmd, exec_path) in cmd_execpath_map.values():
-        with Popen(cmd.split(), cwd=exec_path, stdout=PIPE, stderr=STDOUT, encoding='utf-8') as proc:
+        with Popen(cmd.split(), cwd=exec_path, stdout=PIPE, stderr=STDOUT,
+                   encoding='utf-8') as proc:
             while proc.stdout:
                 data:str = proc.stdout.read(1024)
                 if not data:
                     break
 
-                logging.info("%s->%s => %s", repo_name, git_branch, data)
+                logging.info("%s->%s => %s", repo_name, _git_branch, data)
 
-if __name__ == '__main__':
+def main():
+    """ Main entrypoint function """
     logging.basicConfig(filename=GLOBALS.LOGGER_FILE, level=logging.DEBUG)
 
     args:argparse.Namespace = parse_arguments()
@@ -118,3 +123,6 @@ if __name__ == '__main__':
 
     for proc in procs:
         proc.join()
+
+if __name__ == '__main__':
+    sys.exit(main())
