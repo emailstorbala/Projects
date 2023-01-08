7dec63c66939e5ebca69f5d46e21b1dc3412db76
Balamurugan R <balamurugan.r@velocix.com>
2023-01-08 09:10:17 +0530

    Added onedark theme and switched to it

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -51,6 +51,7 @@ require('packer').startup(function(use)
   use 'lewis6991/gitsigns.nvim'
 
   use 'morhetz/gruvbox'
+  use 'joshdick/onedark.vim'
   use 'nvim-lualine/lualine.nvim' -- Fancier statusline
   use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
   use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
@@ -124,7 +125,7 @@ vim.o.updatetime = 250
 vim.wo.signcolumn = 'yes'
 
 -- Set colorscheme
-vim.cmd [[colorscheme gruvbox]]
+vim.cmd [[colorscheme onedark]]
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
@@ -160,7 +161,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 require('lualine').setup {
   options = {
     icons_enabled = false,
-    theme = 'gruvbox',
+    theme = 'onedark',
     component_separators = '|',
     section_separators = '',
   },
