ff46ec52e7fee691ac02fd75441a0fd1a5ba9369
Balamurugan R <emailstorbala@gmail.com>
2023-01-21 14:25:26 +0530

    Modified rust implementation of aoc2022 day1p2 implementation

--- aoc_2022/rust_impl/day1p2/src/main.rs
+++ aoc_2022/rust_impl/day1p2/src/main.rs
@@ -1,42 +1,14 @@
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
-
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
-
-fn read_input_file(inp: String) -> Vec<i64> {
-    let mut res = Vec::new();
-    let mut tmp = 0;
-
-    if let Ok(lines) = read_lines(inp) {
-        for line in lines {
-            if let Ok(ip) = line {
-                if ip.is_empty() {
-                    res.push(tmp);
-                    tmp = 0;
-                } else {
-                    tmp += ip.parse::<i64>().unwrap();
-                }
-            }
-        }
-
-        res.push(tmp);
-    }
-
-    return res;
-}
+use std::fs;
 
 fn main() {
-    let inp = String::from("inp_file.txt");
-    let mut ctx = read_input_file(inp);
-    ctx.sort();
-    ctx.reverse();
-    let tot_calorie = ctx[0] + ctx[1] + ctx[2];
-    println!("num is {}", tot_calorie);
+    let contents = fs::read_to_string("inp_file.txt").expect("Unable to load file!");
+    let calories = contents.split("\n\n")
+        .map(|chunk| -> i64 {chunk.split('\n').map(|chunk_rec| chunk_rec.parse().unwrap_or(0)).sum()});
+
+    let mut calorie_coll: Vec<i64> = calories.collect();
+    calorie_coll.sort();
+    calorie_coll.reverse();
+    let res_calorie = calorie_coll[0] + calorie_coll[1] + calorie_coll[2];
+
+    println!("Highest calorie is {}", res_calorie);
 }
