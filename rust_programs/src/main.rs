56c0d5a0c04bcbda54022917827e2c860b55d9b5
Balamurugan R <emailstorbala@gmail.com>
2023-01-22 15:18:41 +0530

    Re-arranged hobby/test rust programs inside the project

---
+++ rust_programs/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let loc = "H W";
+    let my_vec: Vec<char> = loc.split(' ').map(|chunk| -> char {chunk.chars().nth(0).unwrap()}).collect();
+    println!("my_vec elements are {}:{}", my_vec[0], my_vec[1]);
+}
