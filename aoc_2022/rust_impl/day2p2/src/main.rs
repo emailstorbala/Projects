91901c491f7f679cfaedbb708678bc1f96dfbff3
Balamurugan R <balamurugan.r@velocix.com>
2023-01-25 09:48:11 +0530

    Fixed few more places where match syntax can be used in aoc2022 day2p2

--- aoc_2022/rust_impl/day2p2/src/main.rs
+++ aoc_2022/rust_impl/day2p2/src/main.rs
@@ -1,15 +1,15 @@
 use std::{fs, char};
 use std::time::Instant;
 
-const ROCK: i32 = 1;
-const PAPER: i32 = 2;
-const SCISSORS: i32 = 3;
+const ROCK: u32 = 1;
+const PAPER: u32 = 2;
+const SCISSORS: u32 = 3;
 
-const WIN_BONUS: i32 = 6;
-const DRAW_BONUS: i32 = 3;
-const LOSE_BONUS: i32 = 0;
+const WIN_BONUS: u32 = 6;
+const DRAW_BONUS: u32 = 3;
+const LOSE_BONUS: u32 = 0;
 
-fn item_value(item: char) -> i32 {
+fn item_value(item: char) -> u32 {
     match item {
         'A' | 'X' => ROCK,
         'B' | 'Y' => PAPER,
@@ -18,32 +18,30 @@ fn item_value(item: char) -> i32 {
     }
 }
 
-fn get_score(opp: char, mine: char) -> i32 {
-    let mut res: i32 = 0;
+fn get_score(opp: char, mine: char) -> u32 {
+    let mut res: u32 = 0;
 
     let opp_val = item_value(opp);
     let my_val = item_value(mine);
 
-    if my_val == ROCK {
-        // LOSE case
-        if opp_val == ROCK {
-            res += SCISSORS + LOSE_BONUS;
-        } else if opp_val == PAPER {
-            res += ROCK + LOSE_BONUS;
-        } else {
-            res += PAPER + LOSE_BONUS;
-        }
-    } else if my_val == PAPER {
+    match my_val {
+        ROCK => {
+            // LOSE case
+            match opp_val {
+                ROCK => res += SCISSORS + LOSE_BONUS,
+                PAPER => res += ROCK + LOSE_BONUS,
+                _ => res += PAPER + LOSE_BONUS
+            }
+        },
         // DRAW case
-        res += opp_val + DRAW_BONUS;
-    } else {
-        // Winning case
-        if opp_val == ROCK {
-            res += PAPER + WIN_BONUS;
-        } else if opp_val == PAPER {
-            res += SCISSORS + WIN_BONUS;
-        } else {
-            res += ROCK + WIN_BONUS;
+        PAPER => res += opp_val + DRAW_BONUS,
+        _ => {
+            // Winning case - SCISSORS
+            match opp_val {
+                ROCK => res += PAPER + WIN_BONUS,
+                PAPER => res += SCISSORS + WIN_BONUS,
+                _ => res += ROCK + WIN_BONUS
+            }
         }
     }
 
@@ -62,7 +60,7 @@ fn main() {
         }
     }
 
-    let mut tot_score: i32 = 0;
+    let mut tot_score: u32 = 0;
     for pair in ctx {
         tot_score += get_score(pair.0, pair.1);
     }
