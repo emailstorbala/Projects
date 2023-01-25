85dc9241968e9426a4f3929348d70019445257f4
Balamurugan R <balamurugan.r@velocix.com>
2023-01-25 09:12:22 +0530

    Fixed lazy static hashmap with simple implementation of enum

--- aoc_2022/rust_impl/day2p2/src/main.rs
+++ aoc_2022/rust_impl/day2p2/src/main.rs
@@ -1,33 +1,5 @@
-extern crate lazy_static;
-
-use lazy_static::lazy_static;
-use std::collections::HashMap;
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
-
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
-
-fn read_input_file(inp: String) -> Vec<String> {
-    let mut res = Vec::new();
-
-    if let Ok(lines) = read_lines(inp) {
-        for line in lines {
-            if let Ok(ip) = line {
-                let pairs = ip.split(" ").collect();
-                res.push(pairs);
-            }
-        }
-    }
-
-    return res;
-}
+use std::{fs, char};
+use std::time::Instant;
 
 const ROCK: i32 = 1;
 const PAPER: i32 = 2;
@@ -37,27 +9,20 @@ const WIN_BONUS: i32 = 6;
 const DRAW_BONUS: i32 = 3;
 const LOSE_BONUS: i32 = 0;
 
-lazy_static! {
-    static ref OPPONENT: HashMap<char, i32> = {
-        let mut map = HashMap::new();
-        map.insert('A', ROCK);
-        map.insert('B', PAPER);
-        map.insert('C', SCISSORS);
-        map
-    };
-    static ref MINE: HashMap<char, i32> = {
-        let mut map = HashMap::new();
-        map.insert('X', ROCK);
-        map.insert('Y', PAPER);
-        map.insert('Z', SCISSORS);
-        map
-    };
+fn item_value(item: char) -> i32 {
+    match item {
+        'A' | 'X' => ROCK,
+        'B' | 'Y' => PAPER,
+        'C' | 'Z' => SCISSORS,
+        _ => panic!("Invalid item sent"),
+    }
 }
 
 fn get_score(opp: char, mine: char) -> i32 {
     let mut res: i32 = 0;
-    let &opp_val = OPPONENT.get(&opp).unwrap();
-    let &my_val = MINE.get(&mine).unwrap();
+
+    let opp_val = item_value(opp);
+    let my_val = item_value(mine);
 
     if my_val == ROCK {
         // LOSE case
@@ -82,20 +47,26 @@ fn get_score(opp: char, mine: char) -> i32 {
         }
     }
 
-    return res;
+    res
 }
 
 fn main() {
-    let inp = String::from("inp_file.txt");
-    let ctx = read_input_file(inp);
+    let start_time = Instant::now();
+    let contents = fs::read_to_string("inp_file.txt").expect("Unable to load the file!");
+    let mut ctx = Vec::new();
+
+    for rec in contents.split('\n') {
+        if !rec.is_empty() {
+            let pair: (char, char) = (rec.chars().nth(0).unwrap(), rec.chars().nth(2).unwrap());
+            ctx.push(pair);
+        }
+    }
 
     let mut tot_score: i32 = 0;
     for pair in ctx {
-        let chrs: Vec<char> = pair.chars().collect();
-        let opp_item: char = chrs[0];
-        let my_item: char = chrs[1];
-
-        tot_score += get_score(opp_item, my_item);
+        tot_score += get_score(pair.0, pair.1);
     }
     println!("Total score is {}", tot_score);
+    let duration = start_time.elapsed();
+    println!("Total time taken -> {:?} ", duration);
 }
