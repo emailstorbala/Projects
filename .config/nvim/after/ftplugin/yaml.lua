60becf8b5a8802bee885bb6af92286ea544fbdb4
Balamurugan R <emailstorbala@gmail.com>
2023-01-26 13:17:03 +0530

    nvim config for after fileplugins fixed

--- .config/nvim/after/ftplugin/yaml.lua
+++ .config/nvim/after/ftplugin/yaml.lua
@@ -1,6 +1,3 @@
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.expandtab = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
--- .config/nvim/lua/base-setup.lua
+++ .config/nvim/lua/base-setup.lua
@@ -1,5 +1,13 @@
 vim.opt.cursorcolumn = true
+vim.opt.cursorline = true
 vim.opt.clipboard = "unnamedplus"
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.shiftround = true
 
 -- Set colorscheme
 vim.cmd [[colorscheme onedark]]
