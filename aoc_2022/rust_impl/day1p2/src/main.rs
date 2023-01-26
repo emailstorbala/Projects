e7984e883621ea1308b9d179cbeabafefd6f3278
Balamurugan R <emailstorbala@gmail.com>
2023-01-26 21:25:38 +0530

    AOC2022 day1p2 rust implementation fmt done

--- aoc_2022/rust_impl/day1p2/src/main.rs
+++ aoc_2022/rust_impl/day1p2/src/main.rs
@@ -2,8 +2,12 @@ use std::fs;
 
 fn main() {
     let contents = fs::read_to_string("inp_file.txt").expect("Unable to load file!");
-    let calories = contents.split("\n\n")
-        .map(|chunk| -> i64 {chunk.split('\n').map(|chunk_rec| chunk_rec.parse().unwrap_or(0)).sum()});
+    let calories = contents.split("\n\n").map(|chunk| -> i64 {
+        chunk
+            .split('\n')
+            .map(|chunk_rec| chunk_rec.parse().unwrap_or(0))
+            .sum()
+    });
 
     let mut calorie_coll: Vec<i64> = calories.collect();
     calorie_coll.sort();
