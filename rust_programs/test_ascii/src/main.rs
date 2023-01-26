6f1c5e1fce43ecf8fb08d2ea5009c73f9203c781
Balamurugan R <emailstorbala@gmail.com>
2023-01-26 16:09:24 +0530

    Added test_ascii rust project

---
+++ rust_programs/test_ascii/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let chr1: char = 'a';
+    let chr2: char = 'z';
+    let chr3: char = 'A';
+    let chr4: char = 'Z';
+
+    println!("chr1 is {}", chr1 as u8);
+    println!("chr2 is {}", chr2 as u8);
+    println!("chr3 is {}", chr3 as u8);
+    println!("chr4 is {}", chr4 as u8);
+}
