70310866a11bca52bac38036c0ded12f793d1abb
Balamurugan R <emailstorbala@gmail.com>
2023-01-26 20:40:52 +0530

    Nvim config fix - Removed vim.opt.cursorbind as it is causing issues

--- .config/nvim/lua/base-setup.lua
+++ .config/nvim/lua/base-setup.lua
@@ -1,7 +1,6 @@
 vim.opt.number = true
 vim.opt.cursorcolumn = true
 vim.opt.cursorline = true
-vim.opt.cursorbind = true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
