577ba19badc5d67f121de6820cab01fe81d60fa0
Balamurugan R <balamurugan.r@velocix.com>
2023-01-27 20:10:13 +0530

    Fixed test_enum rust exercise

--- rust_programs/test_enum/src/main.rs
+++ rust_programs/test_enum/src/main.rs
@@ -1,8 +1,19 @@
-fn main() {
-    let item: char = 'X';
+enum Objects {
+    ROCK = 1,
+    PAPER,
+    SCISSORS
+}
 
-    match item {
-        'A' | 'X' => println!("Item matched!"),
-        _ => println!("Item not matched!")
+fn main() {
+    println!("Rock value is {}", Objects::ROCK as i8);
+    println!("Paper value is {}", Objects::PAPER as i8);
+    println!("Scissors value is {}", Objects::SCISSORS as i8);
+
+    for obj in [Objects::ROCK, Objects::PAPER, Objects::SCISSORS] {
+        match obj {
+            Objects::ROCK => println!("I am rock!"),
+            Objects::PAPER => println!("I am paper!"),
+            Objects::SCISSORS => println!("I am scissors!"),
+        }
     }
 }
