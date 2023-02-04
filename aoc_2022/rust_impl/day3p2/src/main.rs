3046590967a80e8c63e15ea4633a481c52879e34
Balamurugan R <emailstorbala@gmail.com>
2023-02-04 20:39:52 +0530

    Improvised AOC2022 rust implementation of day3p2

--- aoc_2022/rust_impl/day3p2/src/main.rs
+++ aoc_2022/rust_impl/day3p2/src/main.rs
@@ -1,22 +1,32 @@
 use std::time::Instant;
 use std::{fs, u32};
 
-fn get_compartment_details(file_contents: &str) -> Vec<Vec<String>> {
-    let mut compartment_details: Vec<Vec<String>> = Vec::new();
-
+fn get_group_common_items(file_contents: &str) -> String {
+    let mut total_common_chars: String = String::from("");
     let mut tmp_grp: Vec<String> = Vec::new();
 
     for chunk_rec in file_contents.split('\n') {
         if !chunk_rec.is_empty() {
             tmp_grp.push(chunk_rec.clone().to_string());
             if tmp_grp.len() == 3 {
-                compartment_details.push(tmp_grp.clone());
+                let comp1: String = tmp_grp.iter().nth(0).unwrap().to_string();
+                let comp2: String = tmp_grp.iter().nth(1).unwrap().to_string();
+                let comp3: String = tmp_grp.iter().nth(2).unwrap().to_string();
+
+                let mut common_chars: String = String::from("");
+                for item in comp1.chars() {
+                    if !common_chars.contains(item) && comp2.contains(item) && comp3.contains(item)
+                    {
+                        common_chars.push(item);
+                    }
+                }
+                total_common_chars += &common_chars;
                 tmp_grp.clear();
             }
         }
     }
 
-    compartment_details
+    total_common_chars
 }
 
 fn get_item_priority(item: char) -> u32 {
@@ -38,27 +48,9 @@ fn main() {
     let start_time = Instant::now();
     let contents = fs::read_to_string("inp_file.txt").expect("Unable to load the file!");
 
-    let compartment_details = get_compartment_details(&contents);
-    let mut total_common_chars: String = String::from("");
+    let total_common_chars: String = get_group_common_items(&contents);
+
     let mut total_priority: u32 = 0;
-
-    for comps in compartment_details {
-        let comp1: String = comps.iter().nth(0).unwrap().to_string();
-        let comp2: String = comps.iter().nth(1).unwrap().to_string();
-        let comp3: String = comps.iter().nth(2).unwrap().to_string();
-
-        // println!("comp_grp is -> {:?}", comp_grp);
-        let mut common_chars: String = String::from("");
-        for item in comp1.chars() {
-            if !common_chars.contains(item) && comp2.contains(item) {
-                if comp3.contains(item) {
-                    common_chars.push(item);
-                }
-            }
-        }
-        total_common_chars += &common_chars;
-    }
-
     for item in total_common_chars.chars() {
         total_priority += get_item_priority(item);
     }
