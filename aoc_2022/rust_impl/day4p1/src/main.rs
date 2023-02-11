f6559a87b0c83b2d583d3581cc11bf4d55a83109
Balamurugan R <emailstorbala@gmail.com>
2023-02-11 19:30:59 +0530

    AOC2022 rust day4p1 - cosmetic issue fixed

--- aoc_2022/rust_impl/day4p1/src/main.rs
+++ aoc_2022/rust_impl/day4p1/src/main.rs
@@ -7,7 +7,6 @@ use std::time::Instant;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// Name of the person to greet
     #[arg(short, long)]
     file_name: String,
 }
